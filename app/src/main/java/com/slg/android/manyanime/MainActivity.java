ada7dda76eda463b1450915566925387e9626d1f
Giotto1 <1135501551@qq.com>
2015-03-09 22:01:42 +0800

    Create
    项目创建

---
+++ app/src/main/java/com/slg/android/manyanime/MainActivity.java
@@ -0,0 +1,22 @@
+package com.slg.android.manyanime;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+
+
+}
